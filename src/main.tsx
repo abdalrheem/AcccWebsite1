f4bef644a76b1fcde0c0e74610fe6c2174178e1a
Abdalrheem <abdalrheem.a1995@gmail.com>
2023-10-29 16:35:41 +0400

    installing chachkra

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";a
+import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App";
 import "./index.css";
